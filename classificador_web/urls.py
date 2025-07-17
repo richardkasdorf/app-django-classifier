14970fa8352337cae8dc3e5dc36b8d95aaae26a0
RichardKasdorf <richardkasdorf1987@gmail.com>
2025-07-17 20:41:54 -0300

    Ajusta dependências para produção 2

--- classificador_web/urls.py
+++ classificador_web/urls.py
@@ -16,6 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Deploy Django funcionando!?")
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
