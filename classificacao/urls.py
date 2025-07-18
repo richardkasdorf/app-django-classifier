615edbe6e3c038dd8b372240f4db196faef2ce77
RichardKasdorf <richardkasdorf1987@gmail.com>
2025-07-17 21:07:40 -0300

    Alterado urls.py

--- classificacao/urls.py
+++ classificacao/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path
 from .views import upload_file
+from django.http import HttpResponse
+
+def home_view(request):
+    return HttpResponse("Página de aplicação funcionando !!")
 
 urlpatterns = [
-    path('', upload_file, name='upload_file'),
+    path('', home_view),
 ]
+
+
+
+
